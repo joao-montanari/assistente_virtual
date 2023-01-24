e23de007b97de451388802e773cc043ab7056033
joao-montanari <joao.unipbr@gmail.com>
2023-01-24 15:33:19 -0300

    aprimoramento e desenvolvimento da aprendizagem por NLU

--- nlu/model.py
+++ nlu/model.py
@@ -1,5 +1,10 @@
 import yaml
 import numpy as np
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Embedding
+from tensorflow.keras.utils import to_categorical
+
 
 data = yaml.safe_load(open('nlu\\train.yml', 'r', encoding='utf-8').read())
 
@@ -10,34 +15,54 @@ for command  in data['commands']:
     outputs.append(f"{command['entity']}\{command['action']}")
 
 # Processar texto: palavras, caracteres, bytes, sub-palavras
-chars = set()
-for input in inputs + outputs:
-    for ch in input:
-        if ch not in chars:
-            chars.add(ch)
 
-# Mapear char-idx
-chr2idx = {}
-idx2chr = {}
 
-for i, ch in enumerate(chars):
-    chr2idx[ch] = i
-    idx2chr[i] = ch
+max_seq = max([len(bytes(x.encode('utf-8'))) for x in inputs])
 
-max_seq = max([len(x) for x in inputs])
-
-print('Número de chars: ', len(chars))
 print('Maior seq: ', max_seq)
 
 # Criar dataset one-hot (número de exemplos, tamanho da seq, num caracteres)
 # Criar dataset disperso (número de exemplos, tamanho da seq)
 
+# Input Data one-hot encoding
+input_data = np.zeros((len(inputs), max_seq, 256), dtype='float32')
+for i, inp in enumerate(inputs):
+    for k, ch in enumerate(bytes(inp.encode('utf-8'))):
+        input_data[i, k, int(ch)] = 1.0
+
+#input Data Sparse
+'''
 input_data = np.zeros((len(inputs), max_seq, len(chars)), dtype='int32')
 
 for i, input in enumerate(inputs):
     for k, ch in enumerate(input):
-        input_data[i, k, chr2idx[ch]] = 1.0
+        input_data[i, k] = chr2idx[ch]
+'''
 
-print(input_data[0])
+#Output Data
+labels = set(outputs)
+
+label2idx = {}
+idx2label = {}
+
+for idx, label in enumerate(labels):
+    label2idx[label] = idx
+    idx2label[idx] = label
+
+output_data = []
+
+for output in outputs:
+    output_data.append(label2idx[output])
+
+output_data = to_categorical(output_data, len(output_data))
+
+print(output_data[0])
+
+model = Sequential()
+model.add(LSTM(128))
+model.add(Dense(len(output_data), activation='softmax'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+model.fit(input_data, output_data, epochs=16)
 
 # Criar o dataset
